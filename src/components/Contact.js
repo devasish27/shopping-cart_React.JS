e693169ad6b7c2bd8f8f4cd662e030445e2e4588
devasish sai pothumudi <126960187+devasish27@users.noreply.github.com>
2024-08-01 11:28:35 +0530

    Add files via upload

---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+// Contact.js
+
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="contact">
+      <h2>Contact Us</h2>
+      <p>If you have any questions or feedback, feel free to reach out to us!</p>
+      <ul>
+        <li>Email: devasishsai2004@gmail.com</li>
+        <li>Phone: +91 891-947-4533</li>
+        <li>Address: GMR Institute of technology, Rajam, India</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Contact;
